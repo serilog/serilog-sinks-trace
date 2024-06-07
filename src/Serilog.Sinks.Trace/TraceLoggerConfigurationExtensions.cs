2d8db0990f7391147efd2ed22dec322aea624211
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-06-07 16:20:56 +1000

    Update to Serilog 4, tidy up

--- src/Serilog.Sinks.Trace/TraceLoggerConfigurationExtensions.cs
+++ src/Serilog.Sinks.Trace/TraceLoggerConfigurationExtensions.cs
@@ -12,71 +12,72 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 using System;
 using Serilog.Configuration;
 using Serilog.Core;
 using Serilog.Events;
 using Serilog.Formatting.Display;
-using Serilog.Sinks.DiagnosticTrace;
 using Serilog.Formatting;
 using Serilog.Formatting.Json;
+using Serilog.Sinks.Trace;
 
-namespace Serilog
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedType.Global
+
+namespace Serilog;
+
+/// <summary>
+/// Adds the WriteTo.Trace() extension method to <see cref="LoggerConfiguration"/>.
+/// </summary>
+public static class TraceLoggerConfigurationExtensions
 {
+    const string DefaultOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}";
+
     /// <summary>
-    /// Adds the WriteTo.Trace() extension method to <see cref="LoggerConfiguration"/>.
+    /// Write log events to the <see cref="System.Diagnostics.Trace"/>.
     /// </summary>
-    public static class TraceLoggerConfigurationExtensions
+    /// <param name="sinkConfiguration">Logger sink configuration.</param>
+    /// <param name="restrictedToMinimumLevel">The minimum level for
+    /// events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+    /// <param name="levelSwitch">A switch allowing the pass-through minimum level
+    /// to be changed at runtime.</param>
+    /// <param name="outputTemplate">A message template describing the format used to write to the sink.
+    /// the default is "{Timestamp} [{Level}] {Message}{NewLine}{Exception}".</param>
+    /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+    /// <returns>Configuration object allowing method chaining.</returns>
+    public static LoggerConfiguration Trace(
+        this LoggerSinkConfiguration sinkConfiguration,
+        LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+        string outputTemplate = DefaultOutputTemplate,
+        IFormatProvider? formatProvider = null,
+        LoggingLevelSwitch? levelSwitch = null)
     {
-        const string DefaultOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}";
+        if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
+        if (outputTemplate == null) throw new ArgumentNullException(nameof(outputTemplate));
+        var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+        return Trace(sinkConfiguration, formatter, restrictedToMinimumLevel, levelSwitch);
+    }
 
-        /// <summary>
-        /// Write log events to the <see cref="System.Diagnostics.Trace"/>.
-        /// </summary>
-        /// <param name="sinkConfiguration">Logger sink configuration.</param>
-        /// <param name="restrictedToMinimumLevel">The minimum level for
-        /// events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
-        /// <param name="levelSwitch">A switch allowing the pass-through minimum level
-        /// to be changed at runtime.</param>
-        /// <param name="outputTemplate">A message template describing the format used to write to the sink.
-        /// the default is "{Timestamp} [{Level}] {Message}{NewLine}{Exception}".</param>
-        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
-        /// <returns>Configuration object allowing method chaining.</returns>
-        public static LoggerConfiguration Trace(
-            this LoggerSinkConfiguration sinkConfiguration,
-            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
-            string outputTemplate = DefaultOutputTemplate,
-            IFormatProvider formatProvider = null,
-            LoggingLevelSwitch levelSwitch = null)
-        {
-            if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
-            if (outputTemplate == null) throw new ArgumentNullException(nameof(outputTemplate));
-            var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
-            return Trace(sinkConfiguration, formatter, restrictedToMinimumLevel, levelSwitch);
-        }
-
-        /// <summary>
-        /// Write log events to the <see cref="System.Diagnostics.Trace"/>.
-        /// </summary>
-        /// <param name="sinkConfiguration">Logger sink configuration.</param>
-        /// <param name="restrictedToMinimumLevel">The minimum level for
-        /// events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
-        /// <param name="levelSwitch">A switch allowing the pass-through minimum level
-        /// to be changed at runtime.</param>
-        /// <param name="formatter">A custom formatter to apply to the output events. This can be used with
-        /// e.g. <see cref="JsonFormatter"/> to produce JSON output. To customize the text layout only, use the
-        /// overload that accepts an output template instead.</param>
-        /// <returns>Configuration object allowing method chaining.</returns>
-        public static LoggerConfiguration Trace(
-            this LoggerSinkConfiguration sinkConfiguration,
-            ITextFormatter formatter,
-            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
-            LoggingLevelSwitch levelSwitch = null)
-        {
-            if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
-            if (formatter == null) throw new ArgumentNullException(nameof(formatter));
-            return sinkConfiguration.Sink(new TraceSink(formatter), restrictedToMinimumLevel, levelSwitch);
-        }
+    /// <summary>
+    /// Write log events to the <see cref="System.Diagnostics.Trace"/>.
+    /// </summary>
+    /// <param name="sinkConfiguration">Logger sink configuration.</param>
+    /// <param name="restrictedToMinimumLevel">The minimum level for
+    /// events passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+    /// <param name="levelSwitch">A switch allowing the pass-through minimum level
+    /// to be changed at runtime.</param>
+    /// <param name="formatter">A custom formatter to apply to the output events. This can be used with
+    /// e.g. <see cref="JsonFormatter"/> to produce JSON output. To customize the text layout only, use the
+    /// overload that accepts an output template instead.</param>
+    /// <returns>Configuration object allowing method chaining.</returns>
+    public static LoggerConfiguration Trace(
+        this LoggerSinkConfiguration sinkConfiguration,
+        ITextFormatter formatter,
+        LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+        LoggingLevelSwitch? levelSwitch = null)
+    {
+        if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
+        if (formatter == null) throw new ArgumentNullException(nameof(formatter));
+        return sinkConfiguration.Sink(new TraceSink(formatter), restrictedToMinimumLevel, levelSwitch);
     }
 }
